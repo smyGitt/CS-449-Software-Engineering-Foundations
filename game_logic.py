c426f80945dcb1897c0e2d5fe2a17f3e490cbb74
smyGitt <yu.seungmok1111@gmail.com>
2025-10-18 01:39:09 -0500

    Sprint 2 Complete

--- game_logic.py
+++ game_logic.py
@@ -1,3 +1,8 @@
+"""
+    Contains all logic required for the SOS game.
+"""
+
+import tkinter as tk
 from tkinter import ttk
 
 class Tile:
@@ -5,16 +10,11 @@ class Tile:
         Tile type containing the button's instance, coordinates, and the ownwer
         of the tile (the player that placed a letter there), to be used on a gameboard.
     """
-    def __init__(self,
-                button_instance:ttk.Button = None, owner:str = None):
+    def __init__(self, button_instance:ttk.Button = None,
+                 x_coord:int  = None, y_coord:int = None):
         self.button_instance = button_instance
-        self.owner = owner
-
-    def on_click(self):
-        """
-            Function that runs when the tile (button) is pressed.
-        """
-        print("clicked")
+        self.owner = None
+        self.coordinate = (x_coord,y_coord)
 
     def set_button_instance(self, new_button):
         """
@@ -22,9 +22,81 @@ class Tile:
         """
         self.button_instance = new_button
 
+class Player:
+    """
+        Contains player-specific information and functions.
+    """
+    def __init__(self, player_name:str, color:str):
+        self.name = player_name
+        self.color = color
+        self.owned_tile = {"S":[], "O":[]}
+        self.score = 0
+        self.score_variable = tk.IntVar()
+
+    def set_name(self, new_name:str):
+        """
+            Update this player's name with a new one.
+        """
+        self.name = new_name
+    
+    def add_owned_tile(self,letter:str, tile:Tile):
+        """
+            Add the newly acquired Tiles to this player.
+        """
+        self.owned_tile[letter].append(tile)
+
 class SOSGameLogic:
     """
         Includes functions and logic for the SOS game board.
     """
-    def process_turn(self, playerName:str, letterChoice:str, tile:Tile = None):
-        pass
+    def __init__(self):
+        self.gameboard_tile_instance_dict = {}
+        self.player_dict = {1:Player("Blue One", "blue"), 2:Player("Red Two", "red")}
+        
+        self.game_board_dimension_variable = tk.IntVar(value=8)
+        self.current_player_number_variable = tk.IntVar(value=1)
+        self.current_player_name_variable = tk.StringVar(value=self.player_dict[self.current_player_number_variable.get()].name)
+        self.current_letter_variable = tk.StringVar(value="S")
+        
+        self.config_match_type = tk.StringVar(value="casual")
+        self.config_do_random_size = tk.BooleanVar()
+        self.config_do_clickhold = tk.BooleanVar()
+        self.config_do_ai_opponent = tk.BooleanVar()
+
+    def dimension_validate(self):
+        """
+            Validate the dimensions the user has chosen to be within expected ranges.
+            Expected range: 3 to 30, integer.
+        """
+        board_dimension = self.game_board_dimension_variable.get()
+        if board_dimension < 3 or board_dimension > 30:
+            return False
+        return True
+
+    def on_tile_click(self, tile:Tile):
+        """
+            Instructions to handle a specific tile('s button) being clicked. To summarize:
+            1. Change button text and color, then disable the button to prevent any more clicks.
+            2. Give ownership of the Tile to the Player that has clicked it, along with the Tile's letter.
+            3. Then switch turn to the next player, by updating the current player to the next player.
+        """
+        self.__update_button(tile)
+        self.player_dict[self.current_player_number_variable.get()].add_owned_tile(self.current_letter_variable.get(), tile)
+        self.__switch_player()
+
+    def __update_button(self, tile:Tile):
+        tile.button_instance.config(text=self.current_letter_variable.get(),
+                                    state=tk.DISABLED,
+                                    disabledforeground=self.__get_current_player().color
+                                    )
+
+    def __switch_player(self):
+        if self.current_player_number_variable.get() == 1:
+            self.current_player_number_variable.set(2)
+            self.current_player_name_variable.set(self.player_dict[2].name)
+        else:
+            self.current_player_number_variable.set(1)
+            self.current_player_name_variable.set(self.player_dict[1].name)
+
+    def __get_current_player(self):
+        return self.player_dict[self.current_player_number_variable.get()]
--- main.py
+++ main.py
@@ -5,9 +5,7 @@ This module contains the main SOS game and GUI logic.
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox as msgbox
-from generate_element_manager import ElementGenerationManager
-from game_logic import SOSGameLogic as gLogic
-from game_logic import Tile
+from game_logic import SOSGameLogic, Tile
 
 # boilerplate from
 # https://stackoverflow.com/questions/17466561/what-is-the-best-way-to-structure-a-tkinter-application
@@ -20,33 +18,20 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
-        # Set default font and style presets and create ElementGenerationManager.
-        self.e_preset = ElementGenerationManager(
-            self.parent,
-            {
-            "DefaultLFont":("CC Wild Words", 40),
-            "DefaultMFont":("CC Wild Words", 20),
-            "DefaultSFont":("CC Wild Words", 10)
-            },
-            {
-            "padding":5,
-            "background":"#FFFFFF",
-            "foreground":"#000000",
-            "highlightbackground":"#888888"
-            }
-        )
-        
-        # Update the default style preset above, append the default font.
-        self.e_preset.edit_default_style({"font":self.e_preset.return_font_preset("DefaultMFont")})
-
         # Initialize game board dictionary.
         self.game_board_button_dict = {}
 
         # Initialize the title screen.
+        self.game_logic =       SOSGameLogic()
+        self.default_font =     "Times New Roman"
+        self.default_font_dict = {
+            "XLarge_Default":   (self.default_font, 80),
+            "Large_Default":    (self.default_font, 40),
+            "Medium_Default":   (self.default_font, 20),
+            "Small_Default":    (self.default_font, 15),
+            "XSmall_Default":   (self.default_font, 10)
+            }
         self.title_screen()
-
-    def switch_frame(self, prev_page, next_page):
-        pass
 
     def title_screen(self):
         """
@@ -54,93 +39,201 @@ class MainApplication(tk.Frame):
         """
         pad_size = 5
         pad_mult = 10
-        text_font= ("CC Wild Words", 40)
 
+        # SOS Title
         title_frame = tk.Frame(self.parent,padx=pad_size*pad_mult,pady=pad_size*pad_mult)
+        title_frame.columnconfigure([0,1],weight=1)
         ttk.Label(title_frame,
-                  text="S O S",
-                  font=text_font,
-                  padding=pad_size).grid(row=0,column=0,columnspan=2,sticky="ew")
+                  text="S  O  S",
+                  font=self.default_font_dict["XLarge_Default"],
+                  anchor="center",
+                  padding=pad_size
+                  ).grid(row=0,column=0,columnspan=2,sticky="ew")
+
+        # Dimension / Size Change
+        dimension_frame = tk.Frame(title_frame,padx=pad_size,pady=pad_size)
+        ttk.Label(dimension_frame,
+                  text="Board size (3 to 15): ",
+                  font=self.default_font_dict["Small_Default"],
+                  anchor="w"
+                  ).grid(row=0,column=0,sticky="W")
+        ttk.Entry(dimension_frame,
+                  textvariable=self.game_logic.game_board_dimension_variable,
+                  font=self.default_font_dict["Small_Default"],
+                  ).grid(row=0,column=1,sticky="EW")
+        dimension_frame.grid(row=2,column=0,columnspan=2,sticky="EW")
 
         # Checkbutton frame
         checkbox_frame = tk.Frame(title_frame,padx=pad_size,pady=pad_size)
-        ttk.Label(checkbox_frame,text="Game options",anchor="w").grid(row=0,column=0,sticky="ew")
-        self.config_do_random_size = tk.BooleanVar()
-        ttk.Checkbutton(checkbox_frame,
-                        text="Random board size",
-                        variable=self.config_do_random_size).grid(row=1,column=0,sticky="ew")
-        self.config_do_clickhold = tk.BooleanVar()
-        ttk.Checkbutton(checkbox_frame,
+        ttk.Label(checkbox_frame,
+                  text="Game options",
+                  font=self.default_font_dict["Medium_Default"],
+                  anchor="w",
+                  ).grid(row=0,column=0,sticky="ew")
+        tk.Checkbutton(checkbox_frame,
+                        text="Random size",
+                        font=self.default_font_dict["Small_Default"],
+                        anchor="w",
+                        variable=self.game_logic.config_do_random_size
+                        ).grid(row=1,column=0,sticky="ew")
+        tk.Checkbutton(checkbox_frame,
                         text="CLICKHOLD",
-                        variable=self.config_do_clickhold).grid(row=2,column=0,sticky="ew")
-        self.config_do_ai_opponent = tk.BooleanVar()
-        ttk.Checkbutton(checkbox_frame,
-                        text="vs. AI Opponent",
-                        variable=self.config_do_ai_opponent).grid(row=3,column=0,sticky="ew")
+                        font=self.default_font_dict["Small_Default"],
+                        anchor="w",
+                        variable=self.game_logic.config_do_clickhold
+                        ).grid(row=2,column=0,sticky="ew")
+        tk.Checkbutton(checkbox_frame,
+                        text="vs. AI",
+                        font=self.default_font_dict["Small_Default"],
+                        anchor="w",
+                        variable=self.game_logic.config_do_ai_opponent
+                        ).grid(row=3,column=0,sticky="ew")
         checkbox_frame.grid(row=1,column=0,sticky="nsew")
 
         # Radiobutton frame
         radiotitle_frame = tk.Frame(title_frame,padx=pad_size,pady=pad_size)
-        ttk.Label(radiotitle_frame, text="Match type").grid(row=0,column=1,sticky="ew")
-        self.config_match_type = tk.StringVar(value="casual")
-        ttk.Radiobutton(radiotitle_frame,
+        ttk.Label(radiotitle_frame,
+                  text="Match type",
+                  font=self.default_font_dict["Medium_Default"],
+                  anchor="w"
+                  ).grid(row=0,column=0,sticky="ew")
+        tk.Radiobutton(radiotitle_frame,
                         text="Casual",
                         value="casual",
-                        variable=self.config_match_type).grid(row=1,column=1,sticky="ew")
-        ttk.Radiobutton(radiotitle_frame,
+                        variable=self.game_logic.config_match_type,
+                        font=self.default_font_dict["Small_Default"],
+                        anchor="w",
+                        ).grid(row=1,column=0,sticky="ew")
+        tk.Radiobutton(radiotitle_frame,
                         text="Ranked",
                         value="ranked",
-                        variable=self.config_match_type).grid(row=2,column=1,sticky="ew")
+                        variable=self.game_logic.config_match_type,
+                        font=self.default_font_dict["Small_Default"],
+                        anchor="w",
+                        ).grid(row=2,column=0,sticky="ew")
         radiotitle_frame.grid(row=1,column=1,sticky="nsew")
 
-        # Start button, the messagebox is temporary for testing inputs above.
-        ttk.Button(title_frame,text="Start!",command=(lambda: self.game_board(10))).grid(row=2,column=0,columnspan=2,sticky="ew")
-        ttk.Button(title_frame,text="Debug!",command=(
-             lambda:msgbox.showinfo(
-                message=f"Seleted options are: \n\
-                    {self.config_do_random_size.get()} and {self.config_match_type.get()}"
-            ))).grid(row=3,column=0,columnspan=2,sticky="ew")
+        # Start button
+        tk.Button(title_frame,
+                   text="Start!",
+                   font=self.default_font_dict["Medium_Default"],
+                   command=self.__validate_and_start
+                   ).grid(row=3,column=0,columnspan=2,sticky="ew")
         title_frame.grid(row=0,column=0,sticky="nsew")
 
-    def game_board(self, board_dimension:int):
+    def __validate_and_start(self):
+        if self.game_logic.dimension_validate():
+            self.__game_board(self.game_logic.game_board_dimension_variable.get())
+        else:
+            msgbox.showerror("Invalid Dimension", "Please enter a valid board dimension.")
+
+    def __game_board(self, board_dimension:int):
         """
             Creates a game board, very barebones.
         """
-        board_side_length = 32*board_dimension
-        button_side_length = board_side_length//board_side_length # = 1? why is it even here
+        board_side_length = 750
+        button_side_length = 750 // board_dimension
 
         new_window = tk.Toplevel(self.parent)
-        new_window.geometry(f"{board_side_length}x{board_side_length}")
-        if (self.config_do_ai_opponent):
-            new_window.title("SOS Gameboard, vs AI")
-        else:
-            new_window.title("SOS Gameboard, vs Human")
+        new_window.title(f"SOS {board_dimension}x{board_dimension} || Human vs "
+                         + ("AI" if self.game_logic.config_do_ai_opponent.get() else "Human")
+                         + f" || {self.game_logic.config_match_type.get()}")
+        new_window.rowconfigure([0,1],weight=1)
+        new_window.columnconfigure([0],weight=0)
+        new_window.columnconfigure([1],weight=1)
+        new_window.geometry(f"{board_side_length+400}x{(int) (board_side_length)}")
         new_window.resizable(False,False)
-        new_window.rowconfigure(0,weight=1)
-        new_window.columnconfigure(0,weight=1)
-        game_board_frame = tk.Frame(new_window,width=board_side_length,height=board_side_length)
 
+        game_board_frame = tk.Frame(new_window, bg="white")
+        game_board_frame.grid_propagate(False)
         for row_index in range(board_dimension):
-            self.game_board_button_dict[row_index] = {}
+            self.game_logic.gameboard_tile_instance_dict[row_index] = {}
             for column_index in range(board_dimension):
-                new_tile = Tile()
-                new_button = ttk.Button(
+                self.game_logic.gameboard_tile_instance_dict[row_index][column_index] = Tile(x_coord = column_index, y_coord = row_index)
+                new_button = tk.Button(
                         game_board_frame,
-                        text="",
+                        font=(self.default_font,int(500 / board_dimension)),
+                        width=button_side_length,
                         compound="center",
-                        command=(new_tile.on_click)
+                        command=(lambda tile = self.game_logic.gameboard_tile_instance_dict[row_index][column_index]:
+                                 self.game_logic.on_tile_click(tile))
                 )
-                new_tile.set_button_instance(new_button)
-                self.game_board_button_dict[row_index][column_index] = new_tile
+                self.game_logic.gameboard_tile_instance_dict[row_index][column_index].set_button_instance(new_button)
                 
-                self.game_board_button_dict[row_index][column_index].button_instance.grid(
+                self.game_logic.gameboard_tile_instance_dict[row_index][column_index].button_instance.grid(
                     row=row_index,
                     column=column_index,
                     sticky="nsew"
                     )
                 game_board_frame.columnconfigure(column_index,weight=1)
             game_board_frame.rowconfigure(row_index,weight=1)
-        game_board_frame.grid(row=0,column=0,sticky="nsew")
+        game_board_frame.grid(row=0,column=1,sticky="nsew",rowspan=2)
+
+        self.__player_tab(new_window,1).grid(row=0,column=0,sticky="NSEW")
+        self.__player_tab(new_window,2).grid(row=1,column=0,sticky="NSEW")
+        self.__board_control_tab(new_window).grid(row=0,rowspan=2,column=3,sticky="NSEW")
+
+    def __player_tab(self, master:tk.Frame, player_number:int):
+        frame = tk.Frame(master=master, relief="raised", border=2, padx=15, pady=10, width=200)
+        frame.grid_propagate(False)
+        frame.rowconfigure([2],weight=1)
+        frame.columnconfigure([0],weight=1)
+        ttk.Label(master=frame,
+                  text="NAME",
+                  font=self.default_font_dict["Small_Default"],
+                  ).grid(row=0,column=0,sticky="NSEW")
+        ttk.Label(master=frame,
+                  text=self.game_logic.player_dict[player_number].name,
+                  font=self.default_font_dict["Medium_Default"],
+                  foreground=self.game_logic.player_dict[player_number].color
+                  ).grid(row=1,column=0,sticky="NEW")
+        ttk.Label(master=frame,
+                  textvariable=self.game_logic.player_dict[player_number].score_variable,
+                  font=self.default_font_dict["Large_Default"], justify="right", anchor="center",
+                  foreground=self.game_logic.player_dict[player_number].color
+                  ).grid(row=2,column=0,sticky="NSEW")
+        ttk.Label(master=frame,
+                  text="AI" if self.game_logic.config_do_ai_opponent.get() and player_number == 2 else "HUMAN",
+                  font=self.default_font_dict["Small_Default"]
+                  ).grid(row=3,column=0,sticky="SEW")
+        return frame
+    
+    def __board_control_tab(self, master:tk.Frame):
+        frame = tk.Frame(master=master, relief="raised", border=2, padx=15, pady=10, width=200)
+        frame.grid_propagate(False)
+        frame.rowconfigure([1],weight=1)
+        frame.columnconfigure([0],weight=1)
+        ttk.Label(master=frame,
+                  text="TURN",
+                  font=self.default_font_dict["Small_Default"],
+                  ).grid(row=0,column=0,sticky="NEW")
+        ttk.Label(master=frame,
+                  textvariable=self.game_logic.current_player_name_variable,
+                  font=self.default_font_dict["Medium_Default"]
+                  ).grid(row=1,column=0,sticky="NEW")
+        ttk.Label(master=frame,
+                  text="LETTER",
+                  font=self.default_font_dict["Small_Default"]
+                  ).grid(row=2,column=0,sticky="NEW")
+        tk.Radiobutton(master=frame,
+                        text="S",
+                        value="S",
+                        variable=self.game_logic.current_letter_variable,
+                        font=self.default_font_dict["Large_Default"],
+                        borderwidth=3,
+                        indicatoron=False,
+                        pady=44
+                        ).grid(row=3,column=0,sticky="new")
+        tk.Radiobutton(master=frame,
+                        text="O",
+                        value="O",
+                        variable=self.game_logic.current_letter_variable,
+                        font=self.default_font_dict["Large_Default"],
+                        borderwidth=3,
+                        indicatoron=False,
+                        pady=44
+                        ).grid(row=4,column=0,sticky="new")
+        return frame
 
 def initialize_application():
     """initializes the application, but without starting mainloop."""
